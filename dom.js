29a1abf48291a2d793c70898a7aa470ebccf88d8
bhanukosuri <bhanu.kosuri@gmail.com>
2023-03-23 10:21:12 +0530

    Items Bold

--- dom.js
+++ dom.js
@@ -26,7 +26,20 @@
 // console.log(items[1]);
 // items[1].textContent = "Hello 2";
 // items[1].style.fontWeight = "bold";
-var title = document.getElementsByClassName("title");
+// items[1].style.backgroundColor = "yellow";
+
+// for (var i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = "#f4f4f4";
+// }
+// var title = document.getElementsByClassName("title");
 // console.log(title[0]);
-title[0].style.fontWeight = "bold";
-title[0].style.color = "green";
+// title[0].style.fontWeight = "bold";
+// title[0].style.color = "green";
+
+var items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+items[2].style.backgroundColor = "green";
+for (var i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+}
+items.style.fontWeight = "bold";
