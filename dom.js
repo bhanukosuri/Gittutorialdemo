cad8d95a84e577ef3e49918b00833eb789037e8b
bhanukosuri <bhanu.kosuri@gmail.com>
2023-03-24 12:00:45 +0530

    List item background color change

--- dom.js
+++ dom.js
@@ -64,14 +64,31 @@
 // var secondItem = document.querySelector('.list-group-item:nth-child(2)');
 // secondItem.style.color = 'coral';
 
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = 'green';
+
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display = 'none';
+
 //QUERY SELECTOR ALL//
 // var titles = document.querySelectorAll('.title');
 
 // console.log(titles);
 // titles[0].textContent = 'Hello';
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = 'green';
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
 
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.display = 'none';
+// for (var i = 0; i < odd.length; i++) {
+//   odd[i].style.backgroundColor = '#f4f4f4';
+//   even[i].style.backgroundColor = '#ccc';
+// }
+
+var listItem = document.querySelectorAll('li');
+// console.log(listItem);
+listItem[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd');
+for (var i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = 'green';
+}
